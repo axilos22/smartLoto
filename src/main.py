ce98aa49cc33ee5d196c612b17c3bcd71aeeb774
Maxell Jea <zenos_22@hotmail.com>
2018-01-02 01:21:26 +0100

    Test scripts
    Test purposes

---
+++ src/main.py
@@ -0,0 +1,25 @@
+import random as rd
+import numpy as np
+
+from ball import Ball
+from pool import Pool
+
+### TEST SECTION ###
+poolSize = 49
+
+#prepare random
+#rd.seed(28469)
+print("One random number")
+rdEl = rd.randrange(0,49,1,int)
+print(rdEl)
+
+print("An array with some random balls")
+rdBalls = np.random.randint(1,poolSize+1,poolSize)
+#print(balls)
+
+balls = np.arange(49)
+balls = [ el+1 for el in balls]
+
+print("Test of the pool")
+poolTest = Pool(balls)
+print(poolTest.balls)
