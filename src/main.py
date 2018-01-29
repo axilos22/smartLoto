e9f351b6d8742a693f0f0d9350de497eb21a9173
Maxell Jea <zenos_22@hotmail.com>
2018-01-29 23:05:43 +0100

    [MOD] Simplification
    Simplication into one script

--- src/main.py
+++ src/main.py
@@ -4,27 +4,36 @@ import numpy as np
 from ball import Ball
 from pool import Pool
 
+### RULES ###
+#Choose 5 numbers between 1 and 49
+#Choose 1 number between 1 and 10
+
 ### TEST SECTION ###
-poolSize = 49
+numberToPick = 5
+minBallNb = 1
+maxBallNb = 49
 
-#prepare random
-#rd.seed(28469)
-print("One random number")
-rdEl = rd.randrange(0,49,1,int)
-print(rdEl)
+minLuckNb = 1
+maxLuckNb = 10
 
-print("An array with some random balls")
-rdBalls = np.random.randint(1,poolSize+1,poolSize)
-#print(balls)
+#Declaring the pool
+pool = np.arange(minBallNb,maxBallNb+1)
+#print(pool)
 
-#Generating the pool of the desired size
-balls = np.arange(49)
-balls = [ el+1 for el in balls]
+luckPool = np.arange(minLuckNb,maxLuckNb)
+#print(luckPool)
 
-print("Test of the pool")
-poolTest = Pool()
-poolTest.fill(balls)
-print(poolTest.balls)
+#Generate random rank
+ranks = np.random.randint(0,maxBallNb+1,numberToPick)
+#print(ranks)
 
-popBall = poolTest.pop(48)
-print(poolTest.balls)
+ball1 = pool[ranks[0]]
+ball2 = pool[ranks[1]]
+ball3 = pool[ranks[2]]
+ball4 = pool[ranks[3]]
+ball5 = pool[ranks[4]]
+
+print("b1={0}, b2={1}, b3={2}, b4={3}, b5={4},"
+    .format(ball1,ball2,ball3,ball4,ball5))
+
+
