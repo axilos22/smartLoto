97ebe8664754959b32a3d4e1fb0c6e3e473de1cd
Maxell <zenos_22@hotmail.com>
2019-03-01 03:40:45 +0100

    [Clean] (Python) Space instead of tabs

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-# GLOBLA import
+# GLOBAL import
 import importlib
 import random as rd
 import numpy as np
@@ -17,18 +17,18 @@ maxLuckNb = 10
 
 ### FUNCTION ###
 def generateRandomDraw():
-	#Declaring the pool
-	pool = np.arange(minBallNb,maxBallNb+1)
-	luckNb = np.random.randint(minLuckNb,maxLuckNb+1)
-	#Generate random rank
-	ranks = np.random.randint(0,maxBallNb+1,numberToPick)
-	#Full random draw
-	ball1 = pool[ranks[0]]
-	ball2 = pool[ranks[1]]
-	ball3 = pool[ranks[2]]
-	ball4 = pool[ranks[3]]
-	ball5 = pool[ranks[4]]
-	print("b1={0}\tb2={1}\tb3={2}\tb4={3}\tb5={4}\tL={5}".format(ball1,ball2,ball3,ball4,ball5,luckNb))
+    #Declaring the pool
+    pool = np.arange(minBallNb,maxBallNb+1)
+    luckNb = np.random.randint(minLuckNb,maxLuckNb+1)
+    #Generate random rank
+    ranks = np.random.randint(0,maxBallNb+1,numberToPick)
+    #Full random draw
+    ball1 = pool[ranks[0]]
+    ball2 = pool[ranks[1]]
+    ball3 = pool[ranks[2]]
+    ball4 = pool[ranks[3]]
+    ball5 = pool[ranks[4]]
+    print("b1={0}\tb2={1}\tb3={2}\tb4={3}\tb5={4}\tL={5}".format(ball1,ball2,ball3,ball4,ball5,luckNb))
 
 testDraw = Drawing("01/01/1990",[1,2,3,4,5],9)
 testDraw1 = Drawing("02/01/1990",[5,4,3,2,1],1)
